09567d4657a3548fff7b106cbc7c1e1628364920
bagachvlad <ssergey2002s@gmail.com>
2022-01-15 20:31:02 +0300

    Initial commit

---
+++ src/main/java/com/filestorage/FilestorageApplication.java
@@ -0,0 +1,14 @@
+package com.filestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilestorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilestorageApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/filestorage/FilestorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilestorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
