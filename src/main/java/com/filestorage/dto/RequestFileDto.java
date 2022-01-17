f98e24a7f42564998de6958327ad8347092fd6e1
bagachvlad <ssergey2002s@gmail.com>
2022-01-17 16:50:51 +0300

    [DTO] Added request and response dto class

---
+++ src/main/java/com/filestorage/dto/RequestFileDto.java
@@ -0,0 +1,23 @@
+package com.filestorage.dto;
+
+public class RequestFileDto {
+
+   private String name;
+   private int size;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+}
